d46b1d14a537ed71f539c1f841f6a6a53bb28e9e
AswathyKumar03 <achu3592@gmail.com>
2023-11-12 15:02:13 -0500

    Removed unneccessary comments

--- src/assignment/DiscountMain.java
+++ src/assignment/DiscountMain.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class DiscountMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		DiscountCoupon discount = new DiscountCoupon();
 		
 		Scanner sc = new Scanner (System.in);
--- src/assignment/LicenceMain.java
+++ src/assignment/LicenceMain.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class LicenceMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		// object creation
 		Licence licence = new Licence();
 		Scanner sc = new Scanner (System.in);
